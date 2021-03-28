1a59a53625e5e7a164faea7de0607823caf735b2
Michael Große <mic.grosse@googlemail.com>
2021-03-28 13:48:30 +0200

    chore: initial commit, showcasing the problem

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<div v-if="manyItems.length">
+		<HelloWorld
+			v-for="item in manyItems"
+			:msg="item.msg"
+		/>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent( {
+	components: {
+		HelloWorld,
+	},
+	computed: {
+		manyItems(): { msg: string }[] {
+			return [ { msg: `item number 1!` } ];
+		},
+	},
+} );
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
